f5834d96eaf5e8394f86031115ae80dc3d5a2336
mjjohans <mjjohans@stud.ntnu.no>
2018-11-07 13:34:57 +0100

    #6 Created own fetch function

--- projectum-quattuor/src/components/Searchbutton.jsx
+++ projectum-quattuor/src/components/Searchbutton.jsx
@@ -5,11 +5,12 @@ const APIQuery = 'https://swapi.co/api/';
 class Searchbutton extends Component {
     constructor(props) {
         super(props);
-        this.state = {value: 'planets/1', data: []};
+        this.state = {value: '', data: []};
     
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
+    
     /*Funcionality to handle form and state of form*/
     /* Changes state of value whenever the form is changed, in realtime. */
     handleChange(event) {
@@ -17,19 +18,21 @@ class Searchbutton extends Component {
     }
     /* Prevents default formsubmit for now, and logs the state that is saved.*/
     handleSubmit(event) {
-        console.log('Be with you, and this was written:' + this.state.data);
+        console.log('Be with you, and this was written:' + this.state.value);
         event.preventDefault();
+        this.handleFetch();
+    }
+
+    /* Method for fecthing from the API. */
+    handleFetch() {
+        fetch(APIQuery + this.state.value)
+        .then(response => response.json())
+        .then(data => this.setState({ data }));
     }
 
     handleJson(json){
         return JSON.stringify(this.state.data);
     }
-    /*Lifecycle method that fetches from API*/
-    componentDidMount() {
-            fetch(APIQuery + this.state.value)
-            .then(response => response.json())
-            .then(data => this.setState({ data }));
-        }
 
 
     render() {
@@ -40,53 +43,12 @@ class Searchbutton extends Component {
                 Search:
                 <input type="text" className="search_bar" value={this.state.value} onChange={this.handleChange} />
             </label>
-            <input type="submit" value="May the Force" />
+            <input type="submit" className="" value="May the Force" />
             </form>  
             {this.handleJson()}
-            json goes here
         </div>
         );
     }
     }
             
 export default Searchbutton;
-/*   constructor(props) {
-    super(props);
-    this.state = {value: ''}
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmet = this.handleSubmit.bind(this);
-}
-    
-handleClick = () => {
-    console.log('The Force may be with you');
-    }
-
-handleChange(event) {
-    this.setState({value: event.target.value});
-}
-
-handleSubmit(event) {
-    console('A term, you have searched.'+ this.state.value + 'it was.')
-    event.preventDefault();
-}
-
-render() {
-    return(
-        <React.Fragment>
-         <div class="search_wrapper">
-            <h1>Search</h1>
-            <form onSubmit={this.handleSubmit}>
-            <FetchData searchTerm="planets/1/"/>
-              <label>
-                <input type="text" class="search_bar" value={this.state.value} onChange={this.handleChange} />
-              </label>
-              <input type="submit" value="May the Force"></input>
-            </form>
-          </div>    
-        </React.Fragment>
-    )
-}
-
-}*/
-
