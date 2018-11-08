d96acfc7c88e5a89be06c03d5e6bb668522995d1
PeterRydberg <peter.rydberg@hotmail.no>
2018-11-08 12:10:06 +0100

    #9 Implemented error and loading handling

--- projectum-quattuor/src/components/Results/ResultSet.js
+++ projectum-quattuor/src/components/Results/ResultSet.js
@@ -10,6 +10,7 @@ export default class ResultSet extends React.Component {
         this.state = {
             resultSetJson: [], // Make sure this is passed down as prop from App after search function
             loading: false,
+            error: null,
         };
 
     }
@@ -17,14 +18,38 @@ export default class ResultSet extends React.Component {
 
     /*Test information, the state should be passed down as a prop from App*/
     componentDidMount() {
-        fetch('https://swapi.co/api/people/?search=skywalker')
-            .then(response => response.json())
-            .then(data => this.setState({ resultSetJson: data.results })) // This would be Luke Skywalker
+        this.loadTestContent();
     }
 
+    loadTestContent(){
+        this.setState({ loading: true });
+
+        fetch('https://swapi.co/api/people/?search=skywalkersa')
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    throw new Error('Request failed');
+                }
+            })
+            .then(data => this.setState({ resultSetJson: data.results, loading: false })) // This would be Luke, Anakin and Schmi Skywalker
+            .catch(error => this.setState({ error, loading: false }));
+    }
 
     render() {
-        const { resultSetJson } = this.state;
+        const { resultSetJson, loading, error } = this.state;
+
+        if(error){
+            return <p>{error}: {error.message}</p>
+        }
+
+        if(loading){
+            return <p>Loading...</p>
+        }
+
+        if(resultSetJson.length < 1){
+            return <p>No results retrieved.</p>
+        }
 
         return (
             <ul className="result_list">
