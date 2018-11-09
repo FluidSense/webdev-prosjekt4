a8f13dd4d689f459c0debc236625a698fe05feda
Peter Rydberg <peter.rydberg@hotmail.no>
2018-11-09 22:23:33 +0100

    Removed example component

--- projectum-quattuor/src/App.js
+++ projectum-quattuor/src/App.js
@@ -7,30 +7,14 @@ import ResultSetContainer from './components/Results/ResultSetContainer';
 import ExampleContainer from './components/example/ExampleContainer';
 
 class App extends Component {
-
-
-
   render() {
-    
     return (
         <div className="App">
-          <header className="App-header">
-            <p>
-              Prosjekt 4
-            </p>
-          </header>
-
-          {/*All divs below should be comprised of their own components*/}
 
           <div className="content">
 
-            <ExampleContainer />
-
             {/*Search bar*/}
             <SearchbuttonContainer />
-
-            {/*Searches are fetched from the database which records the users activity. The objects will only be loaded progressively as the user scrolls.*/}
-
 
             {/*Result set*/}
             <div className="result_wrapper">
