0ad279a4467a4a23e79b02b039c3562461771ec0
mjjohans <mjjohans@stud.ntnu.no>
2018-11-09 18:06:28 +0100

    #6 #8 #9 #10 Tests for UI elements, search, apicall, display of results

---
+++ projectum-quattuor/cypress/integration/UIandAPItest.spec.js
@@ -0,0 +1,59 @@
+describe("Tests for UI elements. Checks if things are reachable, and if filters behave as expected.", function() {
+   
+    it('Attempts to write in the searchbar', function() {
+        cy.visit('localhost:3000')
+        cy.get('.search_bar')
+             .type('Luke, I am your father')
+             .should('have.value', 'Luke, I am your father')
+    })
+   
+    it('Clicks filters, checks if the correct filter is placed, then removes filter. Also checks if switching from one filter to another goes seamlessly.', function() {
+        cy.get('.checkbox:nth-child(1)')
+            .click()
+            cy.get('.search_bar')
+                .should('have.value', 'planet/?search=')
+            cy.get('.checkbox:nth-child(1)')
+                .click()
+                 .should('have.value', '')
+
+        cy.get('.checkbox:nth-child(2)')
+            .click()
+            cy.get('.search_bar')
+                .should('have.value', 'starships/?search=')
+            cy.get('.checkbox:nth-child(2)')
+                .click()
+                .should('have.value', '') 
+
+        cy.get('.checkbox:nth-child(3)')
+            .click()
+            cy.get('.search_bar')
+                .should('have.value', 'people/?search=')
+            cy.get('.checkbox:nth-child(3)')
+                .click()
+                .should('have.value', '')
+    })
+})
+
+describe("Checks if the api is available, then does a testsearch for Skywalker.", function(){
+   it('Sets a filter and searches for characters with the name "Skywalker", then checks if the document body contains the desired results.', function() {
+    cy.get('.checkbox:nth-child(3)')
+        .click()
+    cy.get('.search_bar')
+        .should('have.value', 'people/?search=')
+        .type('Skywalker {enter}')
+    cy.get('.search_wrapper')
+        cy.contains('count')
+        cy.contains('Luke Skywalker')
+        cy.contains('Anakin Skywalker')
+        cy.contains('Shmi Skywalker')
+    it('Checks directly from the API if the same search yields the same response.', function() {
+        cy.request('https://swapi.co/api/people/?search=Skywalker')
+        .then((response) => {
+            expect(response.body).to.have.property('count', 3)
+            expect(response.body.results[0]).to.have.property('name', 'Luke Skywalker')
+            expect(response.body.results[1]).to.have.property('name', 'Anakin Skywalker')
+            expect(response.body.results[2]).to.have.property('name', 'Shmi Skywalker')
+      })  
+    })
+   }) 
+})
