af5bedbc48720636c21640bac0649a80dc80b557
Peter Rydberg <peter.rydberg@hotmail.no>
2018-11-10 00:18:54 +0100

    Started some visual changes

--- projectum-quattuor/src/components/Search/Searchbutton.jsx
+++ projectum-quattuor/src/components/Search/Searchbutton.jsx
@@ -59,66 +59,85 @@ export class Searchbutton extends Component {
 
     render() {
         return (
-        <React.Fragment>
+        <div className="search_content">
         <div className="search_wrapper"> 
-            <form onSubmit={this.handleSubmit} method="#">
+            <form onSubmit={this.handleSubmit} method="#" id="search_form">
                 <label>
                     <input type="text" className="search_bar" value={this.state.searchValue} onChange={this.handleChange} />
                 </label>
-                <div>
-                    <input type="submit" className="search_button" value="May the Force be with you." />
-                </div>
             </form>
+            <button type="submit" form="search_form" className="search_button">May the Force be with you.</button>
         </div>
 
          <div className="checkboxes">
-            <div className="checkbox">
-                <label for="planetBox">Planet
-                    <input
-                        className="checkboxes"
-                        id="planetBox"
-                        type="checkbox"
-                        checked={this.state.isChecked} 
-                        onChange={this.handleCheck}
-                        value="planets/?search="
-                        />
-                </label>
-            </div>
-            <div className="checkbox">
-                <label>Starships
-                    <input
-                        className="checkboxes"
-                        type="checkbox"
-                        checked={this.state.isChecked} 
-                        onChange={this.handleCheck}
-                        value="starships/?search="
-                        />
-                </label>
-            </div>
-            <div className="checkbox">
-                <label>People
-                    <input
-                        className="checkboxes"
-                        type="checkbox"
-                        checked={this.state.isChecked} 
-                        onChange={this.handleCheck}
-                        value="people/?search="
-                        />
-                </label>
-            </div>
+            <label className="check_label" for="planetBox">Planet
+                <input
+                    className="checkboxes"
+                    id="planetBox"
+                    type="checkbox"
+                    checked={this.state.isChecked} 
+                    onChange={this.handleCheck}
+                    value="planets/?search="
+                />
+            </label>
+            <label className="check_label">Starships
+                <input
+                    className="checkboxes"
+                    type="checkbox"
+                    checked={this.state.isChecked} 
+                    onChange={this.handleCheck}
+                    value="starships/?search="
+                />
+            </label>
+            <label className="check_label">People
+                <input
+                    className="checkboxes"
+                    type="checkbox"
+                    checked={this.state.isChecked} 
+                    onChange={this.handleCheck}
+                    value="people/?search="
+                />
+            </label>
+            <label className="check_label">Species
+                <input
+                    className="checkboxes"
+                    id="planetBox"
+                    type="checkbox"
+                    checked={this.state.isChecked}
+                    onChange={this.handleCheck}
+                    value="species/?search="
+                />
+            </label>
+            <label className="check_label">Films
+                <input
+                    className="checkboxes"
+                    type="checkbox"
+                    checked={this.state.isChecked}
+                    onChange={this.handleCheck}
+                    value="films/?search="
+                />
+            </label>
+            <label className="check_label">Vehicles
+                <input
+                    className="checkboxes"
+                    type="checkbox"
+                    checked={this.state.isChecked}
+                    onChange={this.handleCheck}
+                    value="vehicles/?search="
+                />
+            </label>
         </div>
 
         <div className="sort_filters"> {/*These are options that the user can make in order to sort and filter the results. The idea is to make it so that changing the value will automatically perform a new request for the result set.*/}
-            <form>
-                {/*For sorting the returned objects based on user choice*/}
-                <select className="sorting">
-                    <option value="lexicographical">Alphabetically</option>
-                    <option value="by_added_date">By added date</option>
-                    <option value="by_added_date_rev">By added date reversed</option>
-                </select>
-            </form>
+            {/*For sorting the returned objects based on user choice*/}
+            <a href="#" className="sort_button">Choose sort method</a>
+            <ul className="sorting">
+                <li className="sort_optn" href="#" value="lexicographical">Alphabetically</li>
+                <li className="sort_optn" href="#" value="by_added_date">By added date</li>
+                <li className="sort_optn" href="#" value="by_added_date_rev">By added date reversed</li>
+            </ul>
         </div>
-        </React.Fragment>
+        </div>
         );
     }
 }
