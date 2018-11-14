d7a9b1238ce18d1a232026ff6b1d7036050e46a1
PeterRydberg <peter.rydberg@hotmail.no>
2018-11-14 16:00:23 +0100

    #11 #5, Fixed line breaking

--- projectum-quattuor/src/components/Search/SearchbuttonContainer.js
+++ projectum-quattuor/src/components/Search/SearchbuttonContainer.js
@@ -4,8 +4,12 @@ import { fetchSearchData, fetchSearchHistory } from '../../state/search/actions'
 import Searchbutton from './Searchbutton';
 
 
-// eslint-disable-next-line
-const _SearchbuttonContainer = props => <Searchbutton searchWithApi={props.searchWithApi} searchForHistoryApi={props.searchForHistoryApi} />;
+// eslint-disable-next-line no-underscore-dangle
+const _SearchbuttonContainer = props => (
+  <Searchbutton
+    searchWithApi={props.searchWithApi}
+    searchForHistoryApi={props.searchForHistoryApi}
+  />);
 
 /* Callable actions as props */
 const mapDispatchToProps = dispatch => ({
