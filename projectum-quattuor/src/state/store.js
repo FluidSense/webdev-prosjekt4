7597db9a22b9f7c54955a6f17671bbd01570b76f
Johannes Kvamme <johannes.kvamme@ntnu.no>
2018-11-14 11:48:05 +0100

    remove example

--- projectum-quattuor/src/state/store.js
+++ projectum-quattuor/src/state/store.js
@@ -2,13 +2,11 @@ import {
   createStore, combineReducers, applyMiddleware, compose,
 } from 'redux';
 import thunkMiddelware from 'redux-thunk';
-import exampleReducer from './example/reducer';
 import searchReducer from './search/reducer';
 
 
 const reducers = combineReducers({
   search: searchReducer,
-  example: exampleReducer,
 });
 
 // eslint-disable-next-line no-underscore-dangle
