dc6d40e561fd0152a59caba5c7855a1926b77c70
PeterRydberg <peter.rydberg@hotmail.no>
2018-11-14 16:04:39 +0100

    #11 #5, fixed another line break

--- projectum-quattuor/src/components/Results/ResultSetContainer.js
+++ projectum-quattuor/src/components/Results/ResultSetContainer.js
@@ -5,8 +5,14 @@ import {
 } from '../../state/search/selectors';
 import ResultSet from './ResultSet';
 
-// eslint-disable-next-line no-underscore-dangle, max-len
-const _ResultSetContainer = props => <ResultSet resultSetJson={props.resultSetJson} error={props.error} loading={props.loading} searchHistory={props.searchHistory} />;
+// eslint-disable-next-line no-underscore-dangle
+const _ResultSetContainer = props => (
+  <ResultSet
+    resultSetJson={props.resultSetJson}
+    error={props.error}
+    loading={props.loading}
+    searchHistory={props.searchHistory}
+  />);
 
 /* Gets desired props from the state store */
 const mapStateToProps = state => ({
