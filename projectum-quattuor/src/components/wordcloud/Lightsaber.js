fdf201b402cb962721bea2ee6e190db6bc086e60
johannkv <johannes.kvamme@ntnu.no>
2018-11-15 10:45:31 +0100

    edit styling and word sorting for better positioning #11

--- projectum-quattuor/src/components/wordcloud/Lightsaber.js
+++ projectum-quattuor/src/components/wordcloud/Lightsaber.js
@@ -5,10 +5,11 @@ const Lightsabers = (props) => {
 
   // Guarantee 10 only
   const words = queryNames.splice(0, 10);
-  const wordsLeft = words.splice(0, words.length / 2);
-  const wordsRight = words;
-  wordsLeft.sort((a, b) => a.length < b.length);
-  wordsRight.sort((a, b) => a.length < b.length);
+  words.sort((a, b) => a.length < b.length);
+  const wordsLeft = [];
+  const wordsRight = [];
+  words.map((word, i) => (i % 2 === 0 ? wordsLeft.push(word) : wordsRight.push(word)));
+
 
   const shaftStyle = {
     color: '#FFFFFF', display: 'inline', backgroundColor: '#888888', padding: '0 6px 0 6px', borderRadius: '0 6px 6px 0',
@@ -17,18 +18,16 @@ const Lightsabers = (props) => {
     color, display: 'inline', margin: '0.2em', fontSize: '20px', fontWeight: 'bold', textShadow: `0 0 8px ${color}`,
   });
 
-  const leftShaft = <p style={shaftStyle}>{wordsLeft.pop()}</p>;
-  const rightShaft = <p style={shaftStyle}>{wordsRight.pop()}</p>;
+  const leftShaft = <p key="firstWordLeftShaft" style={shaftStyle}>{wordsLeft.pop()}</p>;
+  const rightShaft = <p key="firstWordRightShaft" style={shaftStyle}>{wordsRight.pop()}</p>;
 
   const saber = (wordList, color) => (
-    [wordList.map((word, i) => <p key={i} style={saberStyle(color)}>{word}</p>)]
+    [wordList.map((word, i) => <p key={`${word}+${i}`} style={saberStyle(color)}>{word}</p>)]
   );
 
   const saberLeft = [leftShaft, ...saber(wordsLeft, 'blue')];
   const saberRight = [rightShaft, ...saber(wordsRight, 'red')];
   const lengthOfSaber = wordsLeft.join('').length + wordsLeft.length;
-
-  const overlapMargin = -110;
 
   return (
     <div style={{
@@ -36,7 +35,7 @@ const Lightsabers = (props) => {
     }}
     >
       <div style={{ transform: 'rotate(315deg)', display: 'inline-block' }}>{saberLeft}</div>
-      <div style={{ transform: 'rotate(225deg)', display: 'inline-block', margin: `${overlapMargin}px` }}>{saberRight}</div>
+      <div style={{ transform: 'rotate(225deg)', display: 'inline-block', position: 'absolute' }}>{saberRight}</div>
     </div>
   );
 };
