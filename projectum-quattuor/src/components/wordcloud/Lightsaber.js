07f500e87f56fe29fa85bc5e31581812f997e059
johannkv <johannes.kvamme@ntnu.no>
2018-11-15 15:15:57 +0100

    rewrite styling for lightsaber wordcloud #11

--- projectum-quattuor/src/components/wordcloud/Lightsaber.js
+++ projectum-quattuor/src/components/wordcloud/Lightsaber.js
@@ -12,29 +12,29 @@ const Lightsabers = (props) => {
 
 
   const shaftStyle = {
-    color: '#FFFFFF', display: 'inline', backgroundColor: '#888888', padding: '0 6px 0 6px', borderRadius: '0 6px 6px 0',
+    color: '#FFFFFF', display: 'inline', backgroundColor: '#555555', padding: '0 20px 0 20px', borderRadius: '0 6px 6px 0',
   };
-  const saberStyle = color => ({
-    color, display: 'inline', margin: '0.2em', fontSize: '20px', fontWeight: 'bold', textShadow: `0 0 8px ${color}`,
+  const saberStyle = (color, glow) => ({
+    color, display: 'inline', margin: '0.2em', fontSize: '20px', fontWeight: 'bold', textShadow: `0 0 8px ${glow}`,
   });
 
   const leftShaft = <p key="firstWordLeftShaft" style={shaftStyle}>{wordsLeft.pop()}</p>;
   const rightShaft = <p key="firstWordRightShaft" style={shaftStyle}>{wordsRight.pop()}</p>;
 
-  const saber = (wordList, color) => (
-    [wordList.map((word, i) => <p key={`${word}+${i}`} style={saberStyle(color)}>{word}</p>)]
+  const saber = (wordList, color, glow = color) => (
+    [wordList.map((word, i) => <p key={`${word}+${i}`} style={saberStyle(color, glow)}>{word}</p>)]
   );
 
-  const saberLeft = [leftShaft, ...saber(wordsLeft, 'blue')];
+  const saberLeft = [leftShaft, ...saber(wordsLeft, 'LightSkyBlue', 'Cyan')];
   const saberRight = [rightShaft, ...saber(wordsRight, 'red')];
   const lengthOfSaber = wordsLeft.join('').length + wordsLeft.length;
 
   return (
     <div style={{
-      minHeight: `${lengthOfSaber}em`, maxHeight: '500px', display: 'flex', alignItems: 'center', justifyContent: 'center',
+      minHeight: `${lengthOfSaber}em`, maxHeight: '500px', display: 'flex', alignItems: 'center', justifyContent: 'center', position: 'relative',
     }}
     >
-      <div style={{ transform: 'rotate(315deg)', display: 'inline-block' }}>{saberLeft}</div>
+      <div style={{ transform: 'rotate(315deg)', display: 'inline-block', position: 'absolute' }}>{saberLeft}</div>
       <div style={{ transform: 'rotate(225deg)', display: 'inline-block', position: 'absolute' }}>{saberRight}</div>
     </div>
   );
