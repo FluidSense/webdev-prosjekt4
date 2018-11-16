e68369d8e695f0e5a4b63c0faab32df655379a0e
johannkv <johannes.kvamme@ntnu.no>
2018-11-16 20:45:59 +0100

    add tests for actions #8

--- projectum-quattuor/src/state/search/actions.js
+++ projectum-quattuor/src/state/search/actions.js
@@ -11,7 +11,7 @@ export const fetchSearchData = fetchUrl => (dispatch) => {
   // Updates loading state and informs reducer which url is being fetched
   dispatch(updateSearchContentLoadingState(true, fetchUrl));
 
-  fetch(fetchUrl)
+  return fetch(fetchUrl)
     .then((response) => {
       if (response.ok) {
         return response.json();
@@ -29,7 +29,7 @@ export const fetchSearchData = fetchUrl => (dispatch) => {
 };
 
 export const fetchSearchHistory = fetchUrl => (dispatch) => {
-  fetch(fetchUrl)
+  return fetch(fetchUrl)
     .then((response) => {
       if (response.ok) {
         return response.json();
--- projectum-quattuor/src/test/searchActions.test.js
+++ projectum-quattuor/src/test/searchActions.test.js
@@ -4,7 +4,11 @@ import configureMockStore from 'redux-mock-store';
 import fetchMock from 'fetch-mock';
 import { APIQuery } from '../components/Search/Search';
 
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+
 describe('Search actions', () => {
+    const store = mockStore({});
     it('should create an action to add a search result', () => {
         const result = {};
         const expectedAction = {
@@ -41,10 +45,14 @@ describe('Search actions', () => {
         };
         expect(actions.updateSearchHistorySet(history)).toEqual(expectedAction);
     });
-});
 
-const middlewares = [thunk];
-const mockstore = configureMockStore(middlewares);
+    // Example of another way to use snapshot tests to test actions.
+    it('should dispatch the correct action',() => {
+        store.clearActions();
+        store.dispatch(actions.updateSearchContentLoadingState(true,'url'));
+        expect(store.getActions()).toMatchSnapshot();
+    })
+});
 
 
 describe('Search async action', () => {
@@ -54,17 +62,48 @@ describe('Search async action', () => {
 
     it('creates FETCHED_RESULTS_RECEIVED when fetching search has been successfully done', () => {
         fetchMock.getOnce(APIQuery, {
-            body: { todos:'help' },
+            body: { count:0,rows:[] },
             headers: {'content-type': 'application/json'}
         });
 
         const expectedActions  = [
             { type: 'FETCH_CONTENT_LOADING_IN_PROGRESS', payload: { loading:true, loadingUrl:APIQuery }},
-            { type: 'FETCHED_RESULTS_RECEIVED', payload: param}
+            { type: 'FETCHED_RESULTS_RECEIVED', payload: {count:0, rows:[]}}
         ];
         const store = mockStore({});
 
         return store.dispatch(actions.fetchSearchData(APIQuery)).then(() => {
+            expect(store.getActions()).toEqual(expectedActions);
+        })
+
+    })
+
+    it('creates FETCHED_HISTORY_RECEIVED when fetching history has been successfully done', () => {
+        fetchMock.getOnce(`${APIQuery}/search`, {
+            body: {
+                id: 248,
+                search_url: '/api/people?search=IG-88&limit=4&offset=0&column=id',
+                search_string: 'IG-88',
+                model: 'people',
+                createdAt: '2018-11-16T18:04:49.656Z',
+                updatedAt: '2018-11-16T18:04:49.656Z'
+               },
+            headers: {'content-type': 'application/json'}
+        });
+
+        const expectedActions  = [
+            { type: 'FETCHED_HISTORY_RECEIVED', payload: {
+                id: 248,
+                search_url: '/api/people?search=IG-88&limit=4&offset=0&column=id',
+                search_string: 'IG-88',
+                model: 'people',
+                createdAt: '2018-11-16T18:04:49.656Z',
+                updatedAt: '2018-11-16T18:04:49.656Z'
+              }}
+        ];
+        const store = mockStore({});
+
+        return store.dispatch(actions.fetchSearchHistory(`${APIQuery}/search`)).then(() => {
             expect(store.getActions()).toEqual(expectedActions);
         })
 
